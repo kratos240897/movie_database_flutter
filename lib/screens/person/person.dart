3cc4c3f0f020d4e92ab22fb984e9477ae6795c96
kratos24 <sudharsan.m@tranxitgroup.com>
2022-02-28 12:41:37 +0530

    major changes

--- lib/screens/person/person.dart
+++ lib/screens/person/person.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:get/state_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_database/screens/person/person_controller.dart';
+
+import '../../helpers/constants.dart';
 
 class Person extends StatefulWidget {
   final String title;
@@ -31,7 +34,64 @@ class _PersonState extends State<Person> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Column(),
+        child: Obx(() {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      controller.isLoaded.value == true
+                          ? CircleAvatar(
+                              radius: 60.0,
+                              backgroundImage: NetworkImage(
+                                  Constants.BASE_IMAGE_URL +
+                                      controller.profile.profilePath),
+                            )
+                          : const CircleAvatar(
+                              radius: 60.0,
+                              backgroundImage:
+                                  AssetImage('assets/images/user.png'))
+                    ],
+                  ),
+                ),
+                Wrap(
+                    spacing: 1.0,
+                    runSpacing: 1.0,
+                    direction: Axis.horizontal,
+                    children: List.generate(
+                        controller.profile.alsoKnownAs.length, (index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.amber,
+                                splashFactory: NoSplash.splashFactory,
+                                shape: const StadiumBorder()),
+                            child: Text(controller.profile.alsoKnownAs[index],
+                                style: const TextStyle(color: Colors.black))),
+                      );
+                    })),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 12.0),
+                  child: Text(
+                    controller.profile.biography,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15.0,
+                        wordSpacing: 1.0,
+                        height: 1.4,
+                        fontFamily: GoogleFonts.quicksand().fontFamily),
+                  ),
+                )
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
--- lib/screens/person/person_controller.dart
+++ lib/screens/person/person_controller.dart
@@ -6,13 +6,15 @@ import '../../repo/app_repo.dart';
 class PersonController extends GetxController {
   final error = ''.obs;
   final _utils = Utils();
-  final Rx<ProfileResponse>? profile = null;
+  final isLoaded = false.obs;
+  late final ProfileResponse profile;
   final AppRepository _appRepo = Get.find<AppRepository>();
 
   getPerson(String id) {
     _utils.showLoading();
     _appRepo.getPerson(id).then((value) {
-      profile!.value = value;
+      isLoaded.value = true;
+      profile = value;
       _utils.hideLoading();
     }).onError((error, stackTrace) {
       this.error.value = error.toString();
